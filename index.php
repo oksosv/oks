1f49d8f8cf4889c3599412c6b5a4aff4778e8eea
oksosv <oksosv567@gmail.com>
2024-05-16 15:16:30 +0530

    Add files via upload
    ok

---
+++ index.php
@@ -0,0 +1,7 @@
+<html>
+    <form method="post">
+        <br>UserName :<input type="text" name="un">
+        <br>Password :<input type="password" name="pw">
+        <br><input type="submit">
+    </form>
+</html>
